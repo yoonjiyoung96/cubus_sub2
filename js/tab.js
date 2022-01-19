05b16948ddd474dcbec26b700c2366f024da6066
yoonjiyoung96 <98012652+yoonjiyoung96@users.noreply.github.com>
2022-01-19 17:42:46 +0900

    Add files via upload

---
+++ js/tab.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".tab_content").hide();
+    $("ul.tabs li:first").addClass("active").show();
+    $(".tab_content:first").show();
+    //On Click Event
+    $("ul.tabs li").click(function () {
+        $("ul.tabs li").removeClass("active");
+        $(this).addClass("active");
+        $(".tab_content").hide();
+        var activeTab = $(this).find("a").attr("href");
+        $(activeTab).fadeIn();
+        return false;
+    });
+
+});
